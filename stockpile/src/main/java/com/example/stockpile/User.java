ee5cd19f36db917f627d756b08310489366fb6a3
姚硕 <3101622182@qq.com>
2021-09-11 09:32:20 +0800

    储存

---
+++ stockpile/src/main/java/com/example/stockpile/User.java
@@ -0,0 +1,55 @@
+package com.example.stockpile;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * @Name:yao
+ * @CreateDate: 2021/9/11 9:05
+ * @ProjectName: DS
+ * @Package: com.example.stockpile
+ * @ClassName: User
+ */
+@Entity
+public class User {
+    @Id(autoincrement = true)
+    public Long id;
+    public String name;
+    public String title;
+    public int age;
+    @Generated(hash = 651379411)
+    public User(Long id, String name, String title, int age) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.age = age;
+    }
+    @Generated(hash = 586692638)
+    public User() {
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
