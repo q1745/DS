dc283863d6cd7aa6df08e2ef13485c6fc17f5dd1
q1745 <1745984453@qq.com>
2021-09-23 19:30:52 +0800

    分类完善

--- Sort/src/main/java/com/example/sort/mvp/injection/component/SortComponent.kt
+++ Sort/src/main/java/com/example/sort/mvp/injection/component/SortComponent.kt
@@ -2,13 +2,13 @@ package com.example.sort.mvp.injection.component
 
 import com.example.di.component.ActivityComponent
 import com.example.di.scope.InjectionScope
-import com.example.sort.mvp.injection.module.FirstModule
-import com.example.sort.mvp.injection.module.ViewModule
-import com.example.sort.mvp.view.FirstFragment
+import com.example.sort.mvp.injection.module.*
+import com.example.sort.mvp.view.SortFragment
 import dagger.Component
 
 @InjectionScope
-@Component(dependencies = arrayOf(ActivityComponent::class),modules = arrayOf(ViewModule::class,FirstModule::class))
+@Component(dependencies = arrayOf(ActivityComponent::class),modules = arrayOf(ViewModule::class,FirstModule::class,SecondViewModule::class,
+    SecondModelModule::class))
 interface SortComponent {
-    fun injectSortFragment(fragment:FirstFragment)
+    fun injectSortFragment(fragment:SortFragment)
 }
--- Sort/src/main/java/com/example/sort/mvp/injection/module/SecondModelModule.kt
+++ Sort/src/main/java/com/example/sort/mvp/injection/module/SecondModelModule.kt
@@ -2,26 +2,19 @@ package com.example.sort.mvp.injection.module
 
 import com.example.mvpcore.IModel
 import com.example.sort.mvp.model.FirstModel
+import com.example.sort.mvp.model.SecondModel
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
 import dagger.multibindings.StringKey
 
-
 @Module
-class FirstModule {
+class SecondModelModule {
 
     @IntoMap
-    @StringKey("remoteModel")
+    @StringKey("SecondremoteModel")
     @Provides
-    fun providesFirstRemoteModel():IModel{
-        return FirstModel()
-    }
-
-    @IntoMap
-    @StringKey("localmodel")
-    @Provides
-    fun providesFirstlocalModel():IModel{
-        return FirstModel()
+    fun providesFirstRemoteModel(): IModel {
+        return SecondModel()
     }
 }
